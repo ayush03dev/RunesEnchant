11f724fd8c607c5fd10748a13bbade22382ed1aa
ayush-deotare <ayush2deotare@gmail.com>
2020-08-23 02:25:55 +0530

    New Enchantments

--- src/me/ayush_03/runesenchant/EnchantmentEffect.java
+++ src/me/ayush_03/runesenchant/EnchantmentEffect.java
@@ -1,27 +1,33 @@
 package me.ayush_03.runesenchant;
 
 import me.ayush_03.runesenchant.utils.ExpressionResolver;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import javax.script.ScriptException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class EnchantmentEffect {
 
-    public int getChance(CustomEnchant ce, int level) {
+    public float getChance(CustomEnchant ce, int level) {
         EnchantmentConfig config = ce.getConfig();
         ConfigurationSection section = config.getConfigFile().getConfigurationSection("chance");
 
         if (section == null) return 0;
 
-        int override = config.getConfigFile().getInt(section.getName() + "level_" + level);
+        float override = (float) config.getConfigFile().getDouble(section.getName() + ".level_" + level);
 
         if (override == 0) {
             String expression = config.getConfigFile().getString(section.getName() + ".expression");
             if (expression == null) return 0;
             try {
-                return ExpressionResolver.getInstance().solve(expression);
+                return ExpressionResolver.getInstance().solve(expression.replace("%level%", level + ""));
             } catch (ScriptException e) {
                 e.printStackTrace();
                 System.out.println("Invalid chance expression!");
@@ -56,12 +62,43 @@ public class EnchantmentEffect {
         }
     }
 
-    public int generateRandom() {
-        return new Random().nextInt(100)+1;
+    public Object get(CustomEnchant ce, int level, String path) {
+        FileConfiguration config = ce.getConfig().getConfigFile();
+        if (config == null) return null;
+        return config.get(path);
     }
 
-    public boolean proc(CustomEnchant ce, int level) {
-        if (generateRandom() <= getChance(ce, level)) return true;
-        return false;
+//    public int generateRandom() {
+//        return new Random().nextInt(100)+1;
+//    }
+
+    public static double generateRandomFloat() {
+        double r =  ThreadLocalRandom.current().nextDouble(0, 100);
+        double scale = Math.pow(10, 2);
+        r = Math.round(r * scale) / scale;
+
+        return r;
     }
+
+    public ItemStack[] getArmor(Player p) {
+        List<ItemStack> list = new ArrayList<>(4);
+        for (ItemStack armorContent : p.getInventory().getArmorContents()) {
+            if (armorContent != null) {
+                list.add(armorContent);
+            }
+        }
+
+        return list.toArray(new ItemStack[0]);
+    }
+
+
+
+//    public boolean proc(CustomEnchant ce, int level) {
+//        if (generateRandom() <= getChance(ce, level)) return true;
+//        return false;
+//    }
+public boolean proc(CustomEnchant ce, int level) {
+    if (generateRandomFloat() <= getChance(ce, level)) return true;
+    return false;
+}
 }
--- src/me/ayush_03/runesenchant/effects/PVPEffects.java
+++ src/me/ayush_03/runesenchant/effects/PVPEffects.java
@@ -9,15 +9,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.List;
 import java.util.Map;
 
 public class PVPEffects extends EnchantmentEffect implements Listener {
 
     @EventHandler
-    public void onDamage(EntityDamageByEntityEvent e) {
+    public void weaponListner(EntityDamageByEntityEvent e) {
 
         // TODO: Later check if entity is instance of Player as well...
 
@@ -34,9 +36,110 @@ public class PVPEffects extends EnchantmentEffect implements Listener {
                 if (proc(ce, level)) {
                     int potionLevel = getValue(ce, level, "potion-level");
                     int potionDuration = getValue(ce, level, "potion-duration");
-                    en.addPotionEffect(new PotionEffect(PotionEffectType.POISON, potionDuration * 20, potionLevel-1));
+                    en.addPotionEffect(new PotionEffect(PotionEffectType.POISON, potionDuration * 20, potionLevel - 1));
+                }
+            }
+
+            if (enchants.containsKey(CustomEnchant.CORRUPTION)) {
+                CustomEnchant ce = CustomEnchant.CORRUPTION;
+                int level = enchants.get(ce);
+                if (proc(ce, level)) {
+                    int potionLevel = getValue(ce, level, "potion-level");
+                    int potionDuration = getValue(ce, level, "potion-duration");
+                    en.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, potionDuration * 20, potionLevel - 1));
+                }
+            }
+
+            if (enchants.containsKey(CustomEnchant.TURMOIL)) {
+                CustomEnchant ce = CustomEnchant.TURMOIL;
+                int level = enchants.get(ce);
+                if (proc(ce, level)) {
+                    int potionLevel = getValue(ce, level, "potion-level");
+                    int potionDuration = getValue(ce, level, "potion-duration");
+                    en.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, potionDuration * 20, potionLevel - 1));
+                }
+            }
+
+            if (enchants.containsKey(CustomEnchant.HEX)) {
+                CustomEnchant ce = CustomEnchant.HEX;
+                int level = enchants.get(ce);
+                if (proc(ce, level)) {
+                    int potionLevel = getValue(ce, level, "potion-level");
+                    int potionDuration = getValue(ce, level, "potion-duration");
+                    en.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, potionDuration * 20, potionLevel - 1));
+                }
+            }
+
+            if (enchants.containsKey(CustomEnchant.HOLY_SMITE)) {
+                CustomEnchant ce = CustomEnchant.HOLY_SMITE;
+                int level = enchants.get(ce);
+                if (proc(ce, level)) {
+                    List<String> list = (List<String>) get(ce, level, "remove-potions");
+                    en.getWorld().strikeLightningEffect(en.getLocation());
+                    list.forEach(str -> {
+                        PotionEffectType type = PotionEffectType.getByName(str);
+                        if (type != null) {
+                            en.removePotionEffect(type);
+                        }
+                    });
+                }
+            }
+
+            if (enchants.containsKey(CustomEnchant.PURGE)) {
+                CustomEnchant ce = CustomEnchant.PURGE;
+                int level = enchants.get(ce);
+                if (proc(ce, level)) {
+                    List<String> list = (List<String>) get(ce, level, "remove-potions");
+                    en.getWorld().strikeLightning(en.getLocation());
+                    list.forEach(str -> {
+                        PotionEffectType type = PotionEffectType.getByName(str);
+                        if (type != null) {
+                            en.removePotionEffect(type);
+                        }
+                    });
+                }
+            }
+
+            if (enchants.containsKey(CustomEnchant.BATTLECRY)) {
+                CustomEnchant ce = CustomEnchant.BATTLECRY;
+                int level = enchants.get(ce);
+                if (proc(ce, level)) {
+                    List<String> list = (List<String>) get(ce, level, "remove-potions");
+                    en.getWorld().strikeLightning(en.getLocation());
+                    list.forEach(str -> {
+                        PotionEffectType type = PotionEffectType.getByName(str);
+                        if (type != null) {
+                            damager.removePotionEffect(type);
+                        }
+                    });
                 }
             }
         }
+    }
+
+    public void armorListener(EntityDamageByEntityEvent e) {
+        // TODO: Later check if entity is instance of Player as well...
+
+//        if (e.getDamager() instanceof Player) {
+            LivingEntity en = (LivingEntity) e.getEntity();
+            Player damager = (Player) e.getDamager();
+
+            Player p = (Player) en;
+
+            for (ItemStack armor : getArmor(p)) {
+                ApplicableItem item = new ApplicableItem(armor);
+                Map<CustomEnchant, Integer> enchants = item.getAllCustomEnchantments();
+
+                if (enchants.containsKey(CustomEnchant.DEMONIC_AURA)) {
+                    CustomEnchant ce = CustomEnchant.DEMONIC_AURA;
+                    int level = enchants.get(ce);
+                    if (proc(ce, level)) {
+                        int potionLevel = getValue(ce, level, "potion-level");
+                        int potionDuration = getValue(ce, level, "potion-duration");
+                        en.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, potionDuration * 20, potionLevel -1));
+                    }
+                }
+            }
+//        }
     }
 }
